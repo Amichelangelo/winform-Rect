edb2398c4b03925602edb0b3260bdda3d5e70cf5
殷琪 <dumuqiaotime@outlook.com>
2018-12-02 16:37:22 +0800

    1

---
+++ WindowsFormsForTestUI/ClassforTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsForTestUI
+{
+    class ClassforTest
+    {
+        public ClassforTest()
+        {
+            var br = new BinaryReader(Stream.Null);
+            br.ReadChar();
+        }
+    }
+}
---
+++ WindowsFormsForTestUI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsForTestUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DealControl(null);  //NO.1 
+            Thread th1 = new Thread(new ThreadStart(Th1_proc));
+            th1.Start();
+        }
+        private void Th1_proc()
+        {
+            DealControl(null);  //NO.2 
+        }
+        private void DealControl(object[] args)
+        {
+            //… 
+            if (InvokeRequired)  //NO.3 
+            {
+                Invoke((Action)delegate ()  //NO.4 
+                {
+                    DealControl(args);
+                });
+            }
+            else
+            {
+                //access ui controls directly 
+                //… 
+            }
+        }
+
+    }
+}
